1383bd2f5b66ea308d7062311c4e748685993289
Landon Miller <millerlm012@gmail.com>
2023-01-25 05:30:04 -0600

    Initial commit

---
+++ manjaro_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ABOUT: to install applications and utilities that will be needed/wanted on manjaro
+
+# updating packages and manager
+sudo pacman -Syu
+
+# installing applications from pacman
+sudo pacman -S spotify-launcher discord brave-browser git make docker sqlitebrowser
+
+# YAY
+cd /tmp/
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER ./yay-git
+cd yay-git
+makepkg -si
+
+# updating yay and packages after install
+yay -syu
+
+# VSCODIUM
+yay -S vscodium-bin
+
+# DOCKER
+# https://linuxways.net/manjaro/how-to-install-docker-on-manjaro-linux/
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+# ability to run docker without root
+sudo usermod -aG docker $USER
